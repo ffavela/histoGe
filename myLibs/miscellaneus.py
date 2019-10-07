27a21a8ee7cd9e89680a7b1180f73f5a5109b1b0
Francisco Favela <ffavela@gmail.com>
2019-10-06 20:59:46 -0500

    Increased font size in plot

--- myLibs/miscellaneus.py
+++ myLibs/miscellaneus.py
@@ -1,5 +1,21 @@
 """Just a set of useful functions"""
 import numpy as np
+from matplotlib import pyplot as plt
+
+###### MATPLOTLIB CONF #########################
+SMALL_SIZE = 15
+MEDIUM_SIZE = 22
+BIGGER_SIZE = 30
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+#################################################
 
 def is_float(n):
     try:
