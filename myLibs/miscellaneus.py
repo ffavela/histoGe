380ca7cf3f340e9677b88bc98709f1cb67c14a10
Francisco Favela <ffavela@gmail.com>
2019-10-25 11:45:10 -0500

    simple database for future implementation

--- myLibs/miscellaneus.py
+++ myLibs/miscellaneus.py
@@ -67,3 +67,56 @@ def getRebinedList(myDataList,rebInt):
         theIdx=(len(xVals)//rebInt)*rebInt+res//2
         newXVals=np.append(newXVals,[xVals[theIdx]])
     return [newXVals,newYVals]
+
+
+####### Some basic database values taken from the isonav #######
+####### Will probably use in the future the whole database #####
+
+listStuff=['n','H','He','Li','Be','B','C','N','O','F','Ne',
+           'Na','Mg','Al','Si','P','S','Cl','Ar','K','Ca',
+           'Sc','Ti','V','Cr','Mn','Fe','Co','Ni','Cu','Zn',
+           'Ga','Ge','As','Se','Br','Kr','Rb','Sr','Y','Zr',
+           'Nb','Mo','Tc','Ru','Rh','Pd','Ag','Cd','In','Sn',
+           'Sb','Te','I','Xe','Cs','Ba','La','Ce','Pr','Nd',
+           'Pm','Sm','Eu','Gd','Tb','Dy','Ho','Er','Tm','Yb',
+           'Lu','Hf','Ta','W','Re','Os',
+           'Ir','Pt','Au','Hg','Tl','Pb','Bi','Po','At','Rn',
+           'Fr','Ra','Ac','Th','Pa','U','Np','Pu','Am','Cm',
+           'Bk','Cf','Es','Fm','Md','No','Lr',
+           'Rf','Db','Sg','Bh','Hs','Mt','Ds','Rg',
+           'Cn','Ed','Fl','Ef','Lv','Eh','Ei']
+
+nameDict={'n':"neutron",'H':"Hydrogen","He":"Helium","Li":"Lithium",
+          "Be":"Berillium","B":"Boron","C":"Carbon","N":"Nitrogen",
+          "O":"Oxygen","F":"Fluorine","Ne":"Neon","Na":"Sodium",
+          "Mg":"Magnesium","Al":"Aluminum","Si":"Silicon",
+          "P":"Phosphorus","S":"Sulfur","Cl":"Chlorine","Ar":"Argon",
+          "K":"Potassium","Ca":"Calcium","Sc":"Scandium",
+          "Ti":"Titanium","V":"Vanadium","Cr":"Chromium",
+          "Mn":"Manganese","Fe":"Iron","Co":"Cobalt","Ni":"Nickel",
+          "Cu":"Copper","Zn":"Zinc","Ga":"Gallium","Ge":"Germanium",
+          "As":"Arsenic","Se":"Selenium","Br":"Bromine","Kr":"Krypton",
+          "Rb":"Rubidium","Sr":"Strontium","Y":"Yttrium","Zr":"Zirconium",
+          "Nb":"Niobium","Mo":"Molybdenum","Tc":"Technetium",
+          "Ru":"Ruthenium","Rh":"Rhodium","Pd":"Palladium","Ag":"Silver",
+          "Cd":"Cadmium","In":"Indium","Sn":"Tin","Sb":"Antimony",
+          "Te":"Tellurium","I":"Iodine","Xe":"Xenon","Cs":"Cesium",
+          "Ba":"Barium","La":"Lanthanum","Ce":"Cerium","Pr":"Praseodymium",
+          "Nd":"Neodymium","Pm":"Promethium","Sm":"Samarium",
+          "Eu":"Europium","Gd":"Gadolinium","Tb":"Terbium","Dy":"Dysprosium",
+          "Ho":"Holmium","Er":"Erbium","Tm":"Thulium","Yb":"Ytterbium",
+          "Lu":"Lutetium","Hf":"Hafnium","Ta":"Tantalum","W":"Tungsten",
+          "Re":"Rhenium","Os":"Osmium","Ir":"Iridium","Pt":"Platinum",
+          "Au":"Gold","Hg":"Mercury","Tl":"Thallium","Pb":"Lead",
+          "Bi":"Bismuth","Po":"Polonium","At":"Astatine","Rn":"Radon",
+          "Fr":"Francium","Ra":"Radium","Ac":"Actinium","Th":"Thorium",
+          "Pa":"Protactinium","U":"Uranium","Np":"Neptunium","Pu":"Plutonium",
+          "Am":"Americium","Cm":"Curium","Bk":"Berkellium",
+          "Cf":"Californium","Es":"Einsteinium","Fm":"Fermium","Md":"Mendelevium",
+          "No":"Nobelium","Lr":"Lawrencium","Rf":"Rutherfordium",
+          "Db":"Dubnium","Sg":"Seaborgium","Bh":"Bohrium","Hs":"Hassium",
+          "Mt":"Meitnerium","Ds":"Darmstadtium","Rg":"Roentgenium",
+          "Cn":"Copernicium","Ed":"Ununtrium","Fl":"Flerovium","Ef":"Ununpentium",
+          "Lv":"Livermorium","Eh":"Ununseptium","Ei":"Ununoctium"}
+
+#######################################
