c85a2455f2297d0c7793f0118093a9b3b0d876ed
maumtzmon <glove1106@gmail.com>
2020-01-21 12:06:12 -0600

    uninstall file: REMOVE ALL

---
+++ uninstall.py
@@ -0,0 +1,8 @@
+#NOTE: this program remove all the histoGe directory and it's content
+#Please BACKUP whatever you want on another directory before you run it
+#
+
+import os
+
+os.system('cd ~/.myPrograms && rm histoGe')
+os.system('cd ~/Software && rm -rf histoGe')
